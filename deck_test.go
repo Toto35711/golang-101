2f7508f1d49ca9742f5a5e4186064b843571f3a4
toto35711 <toto@DESKTOP-CN07SJJ.localdomain>
2023-10-22 23:09:31 +0700

    feat: add testing

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -12,6 +15,21 @@ func TestNewDeck(t *testing.T) {
 	if d[0] != "Spades of Ace" {
 		t.Errorf("Expected Spades of Ace for the first card, but got %v", d[0])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
 
 // t.Errorf("Expected deck length of 16, but got %v", len(d))
